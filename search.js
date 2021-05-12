5316e40f90fd50f82b5c4ec4e3afcacdf7853c3d
Serge TALAKI <sergestalaki@gmail.com>
2021-05-12 11:35:14 +0000

    added amin file to themain file
    function that use deep search algo

---
+++ search.js
@@ -0,0 +1,26 @@
+
+const elements = [
+    {
+        name: 'TOGO',
+        cities: ["LOME", "AGOE", "AFLAO"]
+    }, {
+        name: "GHANA",
+        cities: ["TEMA", "EAST LEAGON", "ACCRA"]
+    }
+];
+
+function sortByCity(query){
+	return elements.map((country, index)=>{
+		const list_cities = country.cities.filter((city, index)=>{
+			return city.toLowerCase().indexOf(query.toLowerCase()) !==-1;
+		});
+		
+		if(list_cities.length>0){
+  		const objCopy = Object.assign({}, {name:country.name});
+  		objCopy.cities = list_cities;
+  		return objCopy;
+		}else{
+		  return null;
+		}
+	}).filter((item, index)=>item!=null);
+}
